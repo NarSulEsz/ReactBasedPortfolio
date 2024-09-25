b4ff50f8bf2ba6f35a10f7bee66398169a7193d3
Nariman Suleimenov <162845160+NarSulEsz@users.noreply.github.com>
2024-09-25 19:20:39 -0400

    Edited Portfolio and Resume pages

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -16,7 +16,7 @@ function NavTabs() {
           // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === '/About' ? 'nav-link active' : 'nav-link'}
         >
-          About
+          About Me
         </NavLink>
       </li>
       <li className="nav-item">
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,22 +1,61 @@
 export default function Portfolio() {
+  const projects = [
+    {
+      title: "HTML Seo Refactor",
+      image: "src/assets/HTML_seo_refactor.png",
+      deployedLink: "https://narsulesz.github.io/html-seo-refactor/",
+      githubLink: "https://github.com/NarSulEsz/html-seo-refactor"
+    },
+    {
+      title: "Blog Demo",
+      image: "src/assets/blog_demo.png",
+      deployedLink: "https://narsulesz.github.io/blog-demo/",
+      githubLink: "https://github.com/NarSulEsz/blog-demo"
+    },
+    {
+      title: "Professional README generator",
+      image: "src/assets/readme_generator.png",
+      deployedLink: "https://drive.google.com/file/d/198U7JrrMb0TfnFfllc6n_wXzPQQLDqkb/view",
+      githubLink: "https://github.com/NarSulEsz/Professional_README_Generator"
+    },
+    {
+      title: "SQL tables",
+      image: "src/assets/SQL_tables.png",
+      deployedLink: "https://drive.google.com/file/d/17j3CV8pZi94gJ8vbpzOedqZoqrLmXgD4/view",
+      githubLink: "https://github.com/NarSulEsz/employeesRolesDepartments"
+    },
+    {
+      title: "E-commerce-Back-End",
+      image: "src/assets/E_commece_Back_End.png",
+      deployedLink: "https://drive.google.com/file/d/1UNpsf0Hb2En3HfaCh7FzDDE0wKoXQM_L/view?usp=sharing",
+      githubLink: "https://github.com/NarSulEsz/E-commerce-Back-End"
+    },
+    {
+      title: "Employee Payroll Tracker",
+      image: "src/assets/EmployeePayrollTracker.png",
+      deployedLink: "https://narsulesz.github.io/Employee-Payroll-Tracker/",
+      githubLink: "https://github.com/NarSulEsz/Employee-Payroll-Tracker"
+    }
+  ];
+
   return (
     <div>
-      <h1>My portfolio</h1>
-      <p>
-        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-        sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-        vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-        libero. Class aptent taciti sociosqu ad litora torquent per conubia
-        nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-        lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-        in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-        bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-        est ligula suscipit velit, sed bibendum turpis dui in sapien.
-      </p>
+      <h1>My Portfolio</h1>
+      <div className="portfolio-grid">
+        {projects.map((project, index) => (
+          <div key={index} className="portfolio-item">
+            <h2>{project.title}</h2>
+            <a href={project.deployedLink} target="_blank" rel="noopener noreferrer">
+              <img src={project.image} alt={project.title} />
+            </a>
+            <p>
+              <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
+                GitHub Repository
+              </a>
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
