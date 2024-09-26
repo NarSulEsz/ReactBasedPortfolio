001250a73d781cf505934c201f0687087ae94b54
Nariman Suleimenov <162845160+NarSulEsz@users.noreply.github.com>
2024-09-26 14:52:37 -0400

    Importation of CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 // Bringing in the required import from 'react-router-dom'
 import { Outlet } from 'react-router-dom';
 import Nav from './components/NavTabs';
-import Footer from './pages/Footer'
+import Footer from './pages/Footer';
+import './App.css';
+
 
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
